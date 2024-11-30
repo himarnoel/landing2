d7fd849c2d65ff42e9a027c0246f7b8f6a77c33a
himarnoel <olaimarnoel@gmail.com>
2024-11-30 16:44:02 +0100

    ignore

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-[#EDF5FE] w-full min-h-screen">
+        <div className="w-full flex justify-between  items-start px-8">
+          <img src="logo.png" alt="" className="mt-4"/>
+          <div className="h-[20rem] w-[20rem]  rounded-full bg-[#7BD2E7] -mt-52"></div>
+        </div>
+        <div className="mt-10 flex items-center px-10">
+      <p className="text-xl text-[#4550E4]">Welcome to STEM Teacher Training</p>
+    <img src="side.svg" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
