dfb529f8d558af4679de4ff1a69431884c1c9a1d
himarnoel <olaimarnoel@gmail.com>
2024-11-30 16:49:30 +0100

    stem

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ function App() {
     <>
       <div className="bg-[#EDF5FE] w-full min-h-screen">
         <div className="w-full flex justify-between  items-start px-8">
-          <img src="logo.png" alt="" className="mt-4"/>
+          <img src="logo.png" alt="" className="mt-4" />
           <div className="h-[20rem] w-[20rem]  rounded-full bg-[#7BD2E7] -mt-52"></div>
         </div>
-        <div className="mt-10 flex items-center px-10">
-      <p className="text-xl text-[#4550E4]">Welcome to STEM Teacher Training</p>
-    <img src="side.svg" alt="" />
+        <div className="mt-10 flex items-center justify-between w-full px-64 relative">
+          <p className="text-xl text-[#4550E4]">
+            Welcome to STEM Teacher Training
+          </p>
+          <img src="side.png" alt="" className="w-32" />
         </div>
       </div>
     </>
